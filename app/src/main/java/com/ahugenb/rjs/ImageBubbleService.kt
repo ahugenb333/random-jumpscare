b20f621805e8c9729d90be08a315c57f34493348
Austin Hugenberg <ahugenberg9@gmail.com>
2023-11-17 09:07:00 -0600

    gif is bigger and centered on phone

--- app/src/main/java/com/ahugenb/rjs/ImageBubbleService.kt
+++ app/src/main/java/com/ahugenb/rjs/ImageBubbleService.kt
@@ -1,5 +1,7 @@
 package com.ahugenb.rjs
 
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.ui.Modifier
 import com.torrydo.floatingbubbleview.service.expandable.BubbleBuilder
 import com.torrydo.floatingbubbleview.service.expandable.ExpandableBubbleService
 import com.torrydo.floatingbubbleview.service.expandable.ExpandedBubbleBuilder
@@ -14,15 +16,17 @@ class ImageBubbleService: ExpandableBubbleService() {
     // optional, only required if you want to call minimize()
     override fun configBubble(): BubbleBuilder? {
         return BubbleBuilder(this)
+            .startLocation(45, 40)
             .bubbleCompose {
-                GifBubbleView()
+                GifBubbleView(modifier = Modifier.fillMaxSize())
             }
     }
 
     override fun configExpandedBubble(): ExpandedBubbleBuilder? {
         return ExpandedBubbleBuilder(this)
+            .startLocation(45, 40)
             .expandedCompose {
-                GifBubbleView()
+                GifBubbleView(modifier = Modifier.fillMaxSize())
             }
     }
 }
